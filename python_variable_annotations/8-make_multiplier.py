07c1366015da4ac861edf4b2b3193b6ed3cbb6ba
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-28 16:47:35 -0300

    fix PEP8

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -8,8 +8,8 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
-        This function takes a float multiplier as argument and returns a function
-        that multiplies a float by multiplier.
+        This function takes a float multiplier as argument
+        and returns a function that multiplies a float by multiplier.
 
         Args:
             multiplier: A float.
