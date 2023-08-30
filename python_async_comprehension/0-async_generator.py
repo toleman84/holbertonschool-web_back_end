a75c2ef69a228f6cba67598feef28883454878b3
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-30 13:34:05 -0300

    fix float

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -20,4 +20,4 @@ async def async_generator() -> Generator[float, None, None]:
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.randint(1, 10)
+        yield random.uniform(1, 10)
