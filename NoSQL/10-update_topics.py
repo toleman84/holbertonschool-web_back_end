0c98c501658e8168c31c28f391b5d4b6c8ebde9f
toleman84 <gdponcemarsiglia@gmail.com>
2023-09-06 14:51:31 -0300

    add exercise 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""doc"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+        Updates the topics of a school document based on the name.
+
+        Args:
+            mongo_collection: The pymongo collection object.
+            name (string): The school name to update.
+            topics (list of strings): The list of topics to be updated.
+        Returns:
+            None.
+    """
+    query = {'name': name}
+    update_data = {'$set': {'topics': topics}}
+
+    mongo_collection.update_many(query, update_data)
