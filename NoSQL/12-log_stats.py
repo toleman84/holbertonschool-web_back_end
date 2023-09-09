e79935637dda8b281dd2f4c7b158aa4d7df170af
toleman84 <gdponcemarsiglia@gmail.com>
2023-09-09 13:30:52 -0300

    add exercise 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,3 +2,33 @@
 """
     doc
 """
+
+import pymongo
+
+
+if __name__ == "__main__":
+    # Connect to the MongoDB database
+    client = pymongo.MongoClient()
+    db = client['logs']
+    collection = db['nginx']
+
+    # Get the number of documents in the collection
+    num_logs = collection.count_documents({})
+
+    # Get the number of documents for each HTTP method
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    n_docs_per_method = {}
+    for method in methods:
+        n_docs_per_method[method] = collection\
+            .count_documents({'method': method})
+
+    # Get the number of documents with method=GET and path=/status
+    num_docs_with_get_and_status = collection\
+        .count_documents({'method': 'GET', 'path': '/status'})
+
+    # Print the stats
+    print("{} logs".format(num_logs))
+    print("Methods:")
+    for method, num_docs in n_docs_per_method.items():
+        print(f"\tmethod", method, "\t", num_docs)
+    print(f"{num_docs_with_get_and_status} status check")
