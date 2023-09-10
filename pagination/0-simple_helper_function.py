66a899990e40b4a53a9a0917713b2d422c9ea988
toleman84 <gdponcemarsiglia@gmail.com>
2023-09-09 21:52:59 -0300

    fix exercise 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """doc"""
 
-from typing import Tuple
-
 
 def index_range(page: int, page_size: int):
-    """ Returns a tuple of size two containing a start index and an end index
+    """
+        Returns a tuple of size two containing a start index and an end index
         corresponding to the range of indexes to return in a list
         for those particular pagination parameters.
 
@@ -13,7 +12,7 @@ def index_range(page: int, page_size: int):
             page (_type_): _description_
             page_size (_type_): _description_
     """
-    start_index: int = (page - 1) * page_size
-    end_index: int = page * page_size
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
 
     return (start_index, end_index)
