ef98cc7bb2e77ded6d63a46b698e9d80855bb766
toleman84 <gdponcemarsiglia@gmail.com>
2023-09-09 21:09:43 -0300

    fix exercise 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -15,6 +15,6 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
             page_size (_type_): _description_
     """
     start_index: int = (page - 1) * page_size
-    end_index: int = page * page_size
+    end_index: int = start_index + page_size
 
     return (start_index, end_index)
