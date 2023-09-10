5a8fd9aeb57fb57afc8723fa13d6456d47284fb0
toleman84 <gdponcemarsiglia@gmail.com>
2023-09-09 22:00:42 -0300

    fix exercise 1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """doc"""
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int):
     """
         Returns a tuple of size two containing a start index and an end index
-        corresponding to the range of indexes to return in a list for those
-        particular pagination parameters.
+        corresponding to the range of indexes to return in a list
+        for those particular pagination parameters.
 
         Args:
             page (_type_): _description_
             page_size (_type_): _description_
     """
-    start_index: int = (page - 1) * page_size
-    end_index: int = start_index + page_size
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
 
     return (start_index, end_index)
 
@@ -50,8 +50,8 @@ class Server:
             Returns:
                 List[list]: _description_
         """
-        assert page > 0
-        assert page_size > 0
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         dataset = self.dataset()
         num_rows = len(dataset)
