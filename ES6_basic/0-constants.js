e84cbd20a9a75f2a1c57e7b4b21617bf4ec50b02
toleman84 <gdponcemarsiglia@gmail.com>
2023-09-16 01:01:54 -0300

    fix ex.0

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -6,7 +6,8 @@ export function getLast() {
   return ' is okay';
 };
 export function taskNext() {
-  const combination = 'But sometimes let';
+  let combination = 'But sometimes let';
   combination += getLast();
-  return combination;
+  const finalCombination = combination;
+  return finalCombination;
 };
