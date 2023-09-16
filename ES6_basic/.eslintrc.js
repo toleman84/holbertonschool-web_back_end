939aa7140398dc50bb4a6a85eca04805a5ba5272
toleman84 <gdponcemarsiglia@gmail.com>
2023-09-16 01:10:31 -0300

    fix

--- ES6_basic/.eslintrc.js
+++ ES6_basic/.eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     },
     plugins: ['jest'],
     rules: {
+        "jest/no-conditional-expect/no-conditional-expect-with-ternary": "off",
         'no-console': 'off',
         'no-shadow': 'off',
         'no-restricted-syntax': [
