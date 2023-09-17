47ebfae4b9a008dd8e895be51daeb4daf5fa0f92
toleman84 <gdponcemarsiglia@gmail.com>
2023-09-16 22:17:16 -0300

    fix

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,8 @@
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+  get maxStudentsSize() {
+    return this._maxStudentsSize;
+  }
+}
