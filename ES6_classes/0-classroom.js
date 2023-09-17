23797fdb87d9d4fcc9e1afa711c02c4d94facda1
toleman84 <gdponcemarsiglia@gmail.com>
2023-09-17 18:58:14 -0300

    fix ex.0

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,7 +1,10 @@
 export default class ClassRoom {
   constructor(maxStudentsSize) {
+    /* eslint-disable */
     this._maxStudentsSize = maxStudentsSize;
+    /* eslint-enable */
   }
+
   get maxStudentsSize() {
     return this._maxStudentsSize;
   }
