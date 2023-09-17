910e102eef0a65e4952dd509ecb1b114cda02c4e
toleman84 <gdponcemarsiglia@gmail.com>
2023-09-17 20:25:43 -0300

    add exercise 2

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,46 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    // Verify the type of attributes.
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    }
+    // Store the attributes in underscore variables.
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+  // Getters and setters for each attribute.
+  get name() {
+    return this._name;
+  }
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name;
+  }
+  get length() {
+    return this._length;
+  }
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length;
+  }
+  get students() {
+    return this._students;
+  }
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    }
+    this._students;
+  }
+}
