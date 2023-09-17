8c1419f9558c588ea116fa1ece9f4c69de58a07b
toleman84 <gdponcemarsiglia@gmail.com>
2023-09-17 17:18:22 -0300

    fix ex.8

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,7 +1,7 @@
 function getCurrentYear() {
   const date = new Date();
   return date.getFullYear();
-  }
+}
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
