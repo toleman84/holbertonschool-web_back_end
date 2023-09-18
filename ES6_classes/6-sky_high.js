18746d4b189f3113293af58d69f85780b6115c85
toleman84 <gdponcemarsiglia@gmail.com>
2023-09-17 22:18:17 -0300

    add exercise 6

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,17 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  // Override the evacuationWarningMessage method.
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
