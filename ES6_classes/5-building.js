738c08a1b1997217fbcda42cb6eda372c93f2a3d
toleman84 <gdponcemarsiglia@gmail.com>
2023-09-17 22:11:42 -0300

    fix ex.5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,18 +1,14 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('Sqft must be a number');
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
 
     this._sqft = sqft;
   }
 
+  // getter:
   get sqft() {
     return this._sqft;
-  }
-
-  // Abstract method.
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
