5548adc5c167f5eb5ec4641789b9b8ab176fb2ed
toleman84 <gdponcemarsiglia@gmail.com>
2023-10-04 23:42:32 -0300

    add exercise 0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,3 +1,7 @@
-function getResponseFromAPI() {
-    
+export default function getResponseFromAPI() {
+  // Make an asynchronous HTTP request to the API.
+  return new Promise((resolve) => {
+    // Resolve the promise with the response data.
+    resolve({ status: 200, body: "Success" });
+  });
 }
