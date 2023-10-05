ec554b263550c3fa42d061bd2f165e14c0c960a8
toleman84 <gdponcemarsiglia@gmail.com>
2023-10-04 23:45:41 -0300

    fix ex.0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -2,6 +2,6 @@ export default function getResponseFromAPI() {
   // Make an asynchronous HTTP request to the API.
   return new Promise((resolve) => {
     // Resolve the promise with the response data.
-    resolve({ status: 200, body: "Success" });
+    resolve({ status: 200, body: 'Success' });
   });
 }
