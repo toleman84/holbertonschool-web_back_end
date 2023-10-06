bf1971aaf7d61ea81741de4a5b01a9b8e16be79b
toleman84 <gdponcemarsiglia@gmail.com>
2023-10-06 20:04:18 -0300

    fix

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup (firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
