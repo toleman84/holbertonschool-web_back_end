2c567a9afb5321fc2582623f3e585d0f4161d44d
toleman84 <gdponcemarsiglia@gmail.com>
2023-10-06 18:54:58 -0300

    add exercise 6

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,17 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup (firstName, lastName, fileName) {
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
+  const results = await Promise.allSettled(promises);
+
+  return results.map((result) => {
+    return {
+      status: result.status,
+      value: result.value || result.reason,
+    };
+  });
+}
