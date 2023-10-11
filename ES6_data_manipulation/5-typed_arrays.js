2ed49b38c43d53dc71f5b9d92cc57918f8a9a81b
toleman84 <gdponcemarsiglia@gmail.com>
2023-10-11 12:34:40 -0300

    add exercise 6

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -4,9 +4,9 @@ export default function createInt8TypedArray(length, position, value) {
   }
 
   const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer);
+  const int8Array = new DataView(buffer);
 
-  int8Array[position] = value;
+  int8Array.setInt8(position, value);
 
-  return buffer;
+  return int8Array;
 }
