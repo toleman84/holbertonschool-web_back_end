946fb0e7fc397eb5be534254368ccb7e14670d38
toleman84 <gdponcemarsiglia@gmail.com>
2023-10-11 11:37:04 -0300

    fix ex. 5

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -6,7 +6,7 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8Array = new Int8Array(buffer);
 
-  int8Array[position] = value;
+  int8Array.setInt8(position, value);
 
-  return buffer;
+  return int8Array;
 }
