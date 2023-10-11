9973773d4f89346f3f600bff2ffd90ada379d44f
toleman84 <gdponcemarsiglia@gmail.com>
2023-10-11 12:59:08 -0300

    add exercise 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+  const filteredSet = new Set();
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      filteredSet.add(value.slice(startString.length));
+    }
+  });
+  const filteredValues = Array.from(filteredSet.values());
+  return filteredValues.join('-');
+}
