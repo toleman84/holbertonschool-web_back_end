6fc20d990817ca0139f56b1af2d4e80a400a5cea
toleman84 <gdponcemarsiglia@gmail.com>
2023-10-11 14:14:08 -0300

    add exercise 9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceriesList() {
+  const listGroceries = new Map();
+
+  listGroceries.set('Apples', '10');
+  listGroceries.set('Tomatoes', '10');
+  listGroceries.set('Pasta', '1');
+  listGroceries.set('Rice', '1');
+  listGroceries.set('Banana', '5');
+
+  return listGroceries;
+}
